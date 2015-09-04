9bd98d2cb56d2734d2473464a056107d739a8be3
Jan Wieners <jan@jan-wieners.de>
2015-09-04 12:03:22 +0200

    Updated Angular UI Bootstrap to v0.13.4.

--- js/spoooky.js
+++ js/spoooky.js
@@ -12,7 +12,7 @@
  *  - jQuery 2.1.4
  *  - Bootstrap 3.3.5
  *  - AngularJS 1.4.4
- *  - Angular UI Bootstrap 0.13.3
+ *  - Angular UI Bootstrap 0.13.4
  *  - Underscore.js 1.8.3
  *  - D3.js 3.5.6
  *  - C3.js 0.4.10
