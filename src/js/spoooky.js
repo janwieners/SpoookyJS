3c8be4359cf855fb4dde6b4d25a0f2819b1f3ba6
Jan Wieners <jan@jan-wieners.de>
2016-04-26 14:40:34 +0200

    Update dependencies.

--- src/js/spoooky.js
+++ src/js/spoooky.js
@@ -1,3 +1,5 @@
+
+"use strict";
 /**
  * Spoooky.js - A JavaScript Multiagent Board Game Framework Based On Monte Carlo Methods
  *
@@ -11,14 +13,12 @@
  * http://www.spoookyjs.de, https://github.com/janwieners/spoookyjs
  *
  * Dependencies:
- *  - jQuery 2.2.3
- *  - Bootstrap 3.3.6
- *  - AngularJS 1.5.3
+ *  - AngularJS 1.5.5
  *  - Angular UI Bootstrap 1.3.2
- *  - Underscore.js 1.8.3
+ *  - Bootstrap 3.3.6
  *  - D3.js 3.5.16
  *  - C3.js 0.4.11-rc4
+ *  - jQuery 2.2.3
+ *  - Underscore.js 1.8.3
  **/
-
-"use strict";
 var Spoooky = Spoooky || {};
