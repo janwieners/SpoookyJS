dcf825964c3865277bd229bbf9ffaf60a6e5e367
Jan Wieners <jan@jan-wieners.de>
2016-05-31 16:33:53 +0200

    Update version number.

--- src/js/spoooky.js
+++ src/js/spoooky.js
@@ -4,7 +4,7 @@
  * Spoooky.js - A JavaScript Multiagent Board Game Framework Based On Monte Carlo Methods
  *
  * @author Jan Gerrit Wieners <jan@jan-wieners.de>
- * Version 0.5.0 "The Erlenmeyer Flask" (December 2015)
+ * Version 0.5.1 "The Erlenmeyer Flask" (December 2015)
  *
  * Copyright (c) Jan G. Wieners
  *
